3d7caa1fd4d459f1838fc991ff2bdc6aed6e7586
SethCol99 <sd17colu@siena.edu>
2022-11-21 02:55:24 -0500

    little_thing file added as an experiment

---
+++ first_dj/polls/little_thing.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+#from . import urls 
+
+# Create your views here.
+def index(request):
+
+    
+    return HttpResponse("new text, created in a separate file not directly read from turtorial")
