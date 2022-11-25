cd354b32159a7d0939a67dcd7358c6e3e8ceab04
SethCol99 <sd17colu@siena.edu>
2022-11-25 18:36:46 -0500

    double quotes added to views

--- firstdj_4real/electrons/views.py
+++ firstdj_4real/electrons/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
   
-def fortune(request):
-  return render(request, 'electrons/lil_rundown.html')
+def info(request):
+  return render(request, "electrons/lil_rundown.html")
